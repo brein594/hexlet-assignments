201d88c74f182f57b3eaee10beb30ec92601b091
brein594 <brein594@gmail.com>
2025-03-30 00:01:41 +0300

    patterns half completed

---
+++ java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
@@ -0,0 +1,21 @@
+package exercise;
+import java.lang.StringBuilder;
+// BEGIN
+public class LabelTag implements TagInterface {
+    private TagInterface labelTag;
+    String labelText;
+
+    public LabelTag(String labelText, TagInterface labelTag) {
+        this.labelText = labelText;
+        this.labelTag = labelTag;
+    }
+
+    public String render() {
+        StringBuilder text = new StringBuilder("<label>");
+        text.append(labelText);
+        text.append(labelTag.render());
+        text.append("</label>");
+        return text.toString();
+    }
+}
+// END
