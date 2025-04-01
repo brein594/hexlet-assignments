4704d2f47a852ce32ce77a0b2aa9bd1127a03df0
@hexlet/cli <support@hexlet.io>
2025-04-01 10:50:04 +0300

    submit java-oop-ru/errors

---
+++ java-oop-ru/errors/src/main/java/exercise/NegativeRadiusException.java
@@ -0,0 +1,10 @@
+package exercise;
+
+// BEGIN
+public class NegativeRadiusException extends Exception {
+
+    public NegativeRadiusException(String massage){
+        super (massage);
+    }
+}
+// END
